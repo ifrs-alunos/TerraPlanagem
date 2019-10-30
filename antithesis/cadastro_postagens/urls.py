8b4971232174c36a0c947c1376b8e13f33ef16d9
Yuri <yurijuliano11tx@gmail.com>
2019-10-30 12:50:43 -0300

    aula 30/10

--- antithesis/cadastro_postagens/urls.py
+++ antithesis/cadastro_postagens/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'cadastro_postagens'
 
 urlpatterns = [
-	path('', views.cadastro_postagens, name='cadastro_postagens')
+	path('', views.cadastro_postagens, name='cadastro_postagens'),
+	path('editar_postagem/<int:publicacao_id>/', views.editar_postagem, name='editar_postagem')
 ]
--- antithesis/cadastro_postagens/views.py
+++ antithesis/cadastro_postagens/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from cadastro_postagens.forms import PublicacaoForm
 from login_usuario.forms import LoginForm
+from home.models import Publicacao
 
 def cadastro_postagens(request):
 	if request.user.is_authenticated:
@@ -18,3 +19,12 @@ def cadastro_postagens(request):
 
 		return render(request, 'cadastro_postagens/postagem.html', {'form': form})
 	return redirect('home')
+
+def editar_postagem(request, publicacao_id):
+	publicacao = get_object_or_404(Publicacao, pk=publicacao_id)
+
+	dados = {
+		'publicacao': publicacao,
+	}
+
+	return render(request, 'cadastro_postagens/editar.html', dados)
