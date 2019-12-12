615334edf20cb106d2387fb10ccb51f0c8262ce4
Yuri <yurijuliano11tx@gmail.com>
2019-12-12 13:00:37 -0300

    allowed hosts: all

--- antithesis/antithesis/settings.py
+++ antithesis/antithesis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pindoramapindamoiangabaestousofrendo#ESQUERDOMACHO'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
